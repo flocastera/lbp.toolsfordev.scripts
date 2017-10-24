cbd4ff4dffd2c027a7512e0f63af81bea7cda3e0
Florian CASTERA <florian.castera@accenture.com>
2017-10-24 12:58:33 +0200

    FCA : status script updated : new display

--- status.sh
+++ status.sh
@@ -9,24 +9,27 @@ mergedFilesCount=0
 
 echo
 echo "$(tput setaf 2)Getting status for projects...$(tput sgr 0)"
-
+echo
+# Looping over directories in Workspace path
 for projectPath in `find $WSP_PATH -maxdepth 1 -type d`
 do
+    # Testing if directory pattern matches watched directories
     test=`echo $projectPath | grep -E "$watchPatterns" -c`
 	if [ "$test" == "1" ] ;
 	then
-		cd $projectPath
+		cd $projectPath # Going into project folder to execute Git commands
 
-		result=`git status -s`
-        resultModified=`echo "$result" | grep -E "^(\s)?M.*"`
-        resultAdded=`echo "$result" | grep -E "^(\s)?A.*"`
-        resultDeleted=`echo "$result" | grep -E "^(\s)?D.*"`
-        resultUnknown=`echo "$result" | grep -E "^(\s)?\?.*"`
-		total=$(echo "$result" | sed '/^\s*$/d' | wc -l)
+		result=`git status -s` # Getting general status for project
+        resultModified=`echo "$result" | grep -E "^(\s)?M.*"`   # Getting modified files
+        resultAdded=`echo "$result" | grep -E "^(\s)?A.*"`      # Getting added files
+        resultDeleted=`echo "$result" | grep -E "^(\s)?D.*"`    # Getting deleted files
+        resultUnknown=`echo "$result" | grep -E "^(\s)?\?.*"`   # Getting merged/merging files
+		total=$(echo "$result" | sed '/^\s*$/d' | wc -l)        # Calculating total
 
 		if [ "$1" == "-h" ] ;
 		then
-		    result=`echo "$result" | grep -Ev "AccreditationService|pom\.xml|\.properties|SecurityBouchonConfig|\.classpath|\.settings|\.gitignore"`
+		    # Re-calculate status with some files excluded
+		    result=`echo "$result" | grep -Ev "$excludedProjectPathsPatterns"`
             resultModified=`echo "$result" | grep -E "^(\s)?M.*"`
             resultAdded=`echo "$result" | grep -E "^(\s)?A.*"`
             resultDeleted=`echo "$result" | grep -E "^(\s)?D.*"`
@@ -34,16 +37,16 @@ do
 		    total=$(echo "$result" | sed '/^\s*$/d' | wc -l)
 		fi
 
+        # Display and total calculation
 		if [ "$total" != "0" ] ;
 		then
-            echo
-            echo "$(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
+            echo "[$(tput setaf 1)x$(tput sgr 0)]──$(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
 
 		    let "totalFilesCount = $totalFilesCount + $total"
 
             if [ "$resultAdded" != "" ] ;
             then
-                echo "$(tput setaf 2)$resultAdded$(tput sgr 0)"
+                echo "$(tput setaf 2)`echo "$resultAdded" | sed "s/^/ ╞──/g "`$(tput sgr 0)"
 		        let "addedFilesCount = $addedFilesCount + `echo "$resultAdded" | wc -l`"
             fi
 
@@ -54,42 +57,44 @@ do
 
 		        let "modifiedFilesCount = $modifiedFilesCount + `echo "$resultModified" | wc -l`"
 
+                # Modified files can either be added to commit or not
                 if [ "$added" != "" ] ;
                 then
-                    echo "$(tput setaf 2)$added$(tput sgr 0)"
+                    echo "$(tput setaf 2)`echo "$added" | sed "s/^/ ╞──/g "`$(tput sgr 0)"
                 fi
                 if [ "$notAdded" != "" ] ;
                 then
-                    echo "$(tput setaf 1)$notAdded$(tput sgr 0)"
+                    echo "$(tput setaf 1)`echo "$notAdded" | sed "s/^/ ╞──/g "`$(tput sgr 0)"
                 fi
             fi
 
             if [ "$resultDeleted" != "" ] ;
             then
-                echo "$(tput setaf 2)$resultDeleted$(tput sgr 0)"
+                echo "$(tput setaf 1)`echo "$resultDeleted" | sed "s/^/ ╞──/g "`$(tput sgr 0)"
 		        let "deletedFilesCount = $deletedFilesCount + `echo "$resultDeleted" | wc -l`"
             fi
 
             if [ "$resultUnknown" != "" ] ;
             then
-                echo "$(tput setaf 3)$resultUnknown$(tput sgr 0)"
+                echo "$(tput setaf 3)`echo "$resultUnknown" | sed "s/^/ ╞──/g "`$(tput sgr 0)"
 		        let "mergedFilesCount = $mergedFilesCount + `echo "$resultUnknown" | wc -l`"
             fi
+            echo " ╘────────────────────────────────────────"
 		else
-            echo "No changes in $(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
+		    # Displaying message if nothing change in folder (supports excluded files)
+            echo "[$(tput setaf 2)V$(tput sgr 0)] No changes in $(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
 		fi
 	fi
 done
 
-echo -e "──┬┬───────────────────────────────────────"
-echo "  ||"
+echo -e "──┬─────────────────────────────────"
 if [ "$totalFilesCount" == "0" ] ;
 then
-    echo "  ╘╧════> $(tput setaf 2)Workspace is clean !$(tput sgr 0)"
+    echo "  ╘────> $(tput setaf 2)Workspace is clean !$(tput sgr 0)"
 else
-    echo "  │╞════> $(tput setaf 2)Total$(tput sgr 0)    : $totalFilesCount"
-    echo "  │╞════> $(tput setaf 3)Added$(tput sgr 0)    : $addedFilesCount"
-    echo "  │╞════> $(tput setaf 3)Modified$(tput sgr 0) : $modifiedFilesCount"
-    echo "  │╞════> $(tput setaf 3)Deleted$(tput sgr 0)  : $deletedFilesCount"
-    echo "  ╘╧════> $(tput setaf 3)Merged$(tput sgr 0)   : $mergedFilesCount"
+    echo "  ╞────> $(tput setaf 2)Total$(tput sgr 0)    : $totalFilesCount"
+    echo "  ╞────> $(tput setaf 3)Added$(tput sgr 0)    : $addedFilesCount"
+    echo "  ╞────> $(tput setaf 3)Modified$(tput sgr 0) : $modifiedFilesCount"
+    echo "  ╞────> $(tput setaf 1)Deleted$(tput sgr 0)  : $deletedFilesCount"
+    echo "  ╘────> Merged   : $mergedFilesCount"
 fi
