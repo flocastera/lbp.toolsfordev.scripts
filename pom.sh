9736b00fe88c45f5f4b15e40569d8ddbc69e57cf
Florian CASTERA <florian.castera@accenture.com>
2017-10-24 16:19:30 +0200

    FCA - making prints all the same for pom

--- pom.sh
+++ pom.sh
@@ -3,35 +3,46 @@
 path="$(pwd)"
 if [ "$#" == "0" ] || [ "$1" == "--full" ] || [ "$1" == "-f" ] ;
 then
-    for D in `find $WSP_PATH -maxdepth 1 -type d`
+    echo
+    echo "$(tput setaf 2)Getting POM version for all projects...$(tput sgr 0)"
+    echo "─┬─────────────────────────────────────────"
+    echo " │"
+
+    for projectPath in `find $WSP_PATH -maxdepth 1 -type d`
     do
-        test="$(echo $D | grep -E "$watchPatterns" -c)"
+        test="$(echo $projectPath | grep -E "$watchPatterns" -c)"
         if [ $test -eq 1 ]
         then
-            echo ""
-            echo -n "$(tput setaf 2)$(echo $D | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")$(tput sgr 0) : "
+            cd $projectPath
+		    projectName=$(echo $projectPath | grep -Eo "$projectNamePatterns")
 
-            cd $D
+            echo -n "[$(tput setaf 2)V$(tput sgr 0)]──$(tput setaf 2)$projectName$(tput sgr 0) : "
+
             res="$(grep -E "<version>.*</version>" pom.xml -m 2 | tail -1)"
             res=${res:11}
             res="$(echo $res | sed 's/.\{10\}$//')"
             echo $res | grep --color -E "[0-9]{2,3}\-.*"
             if [ "$1" == "--full" ] || [ "$1" == "-f" ] ;
             then
-                res="$(awk '/<properties>/,/<\/properties>/' pom.xml | grep -Eo '(composant\-applicatif|module)+\-[a-zA-Z0-9]+.*>0[0-9]{1}_[0-9]{2}_[0-9]{2}\.[0-9]{2,3}(\-SNAPSHOT){0,1}' pom.xml | sed 's/.adb.version>/ /g ' | sort  | sed 's/composant-applicatif-/ ╞──/g ')"
+                res="$(awk '/<properties>/,/<\/properties>/' pom.xml | grep -Eo '(composant\-applicatif|module)+\-[a-zA-Z0-9]+.*>0[0-9]{1}_[0-9]{2}_[0-9]{2}\.[0-9]{2,3}(\-SNAPSHOT){0,1}' pom.xml | sed 's/.adb.version>/ /g ' | sort  | sed 's/composant-applicatif-/ ╞───/g ')"
                 if [ $(echo "$res" | sed '/^\s*$/d' | wc -l) -gt 0 ] ;
                 then
                     echo "$res" | grep --color -E '[0-9]{2,3}(\-.*|$)'
-                    echo " ╘─────────────────────────────────────────────"
                 fi
             fi
-            cd ../
+            echo " │"
         fi
     done
+    echo " ╘─────────────────────────────────────────────"
 fi
 if [ "$1" == "--sync" ] || [ "$1" == "-s" ] ;
 then
-    projects=`find $WSP_PATH -maxdepth 1 -type d | grep -E '(module|composants)\-applicatif'`
+    echo
+    echo "$(tput setaf 2)Synchronizing POM dependencies version for all projects...$(tput sgr 0)"
+    echo "─┬─────────────────────────────────────────────────────"
+    echo " │"
+
+    projects=`find $WSP_PATH -maxdepth 1 -type d | grep -E "$watchPatterns"`
     tempFile="$WSP_PATH/tmpFile"
     for project in $projects
     do
@@ -46,13 +57,8 @@ then
 
         if [ $(echo "$dependencies" | sed '/^\s*$/d' | wc -l) -gt 0 ] ;
         then
-            echo
             minProject=`echo $project | grep -Eo '[a-zA-Z0-9-]+$' | grep -Eo '(H[0-9]{2}\-)?[a-zA-Z]*$'`
-            echo "$(tput setaf 2)$minProject$(tput sgr 0)"
-            let "size = ${#minProject} - 3"
-            str="──┬"
-            for ((i=1; i<=$size; i++)); do str="$str─"; done
-            echo $str
+            echo "[$(tput setaf 2)V$(tput sgr 0)]──$(tput setaf 2)$minProject$(tput sgr 0)"
             updated=0
             errors=0
             for dependency in $dependencies
@@ -65,15 +71,15 @@ then
 		            let "updated = $updated + 1"
                     correspondingVersion=`echo "$correspondingProject" | grep -Eo '0[0-9]{1}_[0-9]{2}_[0-9]{2}\.[0-9]{2,3}(\-SNAPSHOT){0,1}'`
                     sed -i -e "s@<$dependency\.adb\.version>.*</$dependency\.adb\.version>@<$dependency.adb.version>${correspondingVersion}<\\/$dependency.adb.version>@g" $chem
-                    echo "  $(tput setaf 2)╞──$(tput sgr 0) $depName updated to $(tput setaf 2)$correspondingVersion$(tput sgr 0)"
+                    echo " $(tput setaf 2)╞───$(tput sgr 0)$depName updated to $(tput setaf 2)$correspondingVersion$(tput sgr 0)"
                 else
 		            let "errors = $errors + 1"
-                    echo "  $(tput setaf 1)╞──$(tput sgr 0) $depName not updated"
+                    echo " $(tput setaf 3)╞───$(tput sgr 0)$depName not updated"
                 fi
             done
-            echo "  │"
-            echo "  ╘───────$(tput setaf 2)Updated( $updated )$(tput sgr 0)──────$(tput setaf 3)Warnings( $errors )$(tput sgr 0)───────"
+            echo " │"
         fi
     done
+    echo " ╘─────────────────────────────────────────────────────"
     rm -rf $tempFile
 fi
