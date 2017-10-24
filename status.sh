0e803f7d9e8fb95db40de94b0ece60152be111ed
Florian CASTERA <florian.castera@accenture.com>
2017-10-24 12:28:46 +0200

    FCA : status script updated to be more verbose and performance improved

--- status.sh
+++ status.sh
@@ -1,43 +1,95 @@
 #!/bin/bash
 
 path="$(pwd)"
+totalFilesCount=0
 modifiedFilesCount=0
+addedFilesCount=0
+deletedFilesCount=0
+mergedFilesCount=0
 
-for D in `find . -maxdepth 1 -type d`
+echo
+echo "$(tput setaf 2)Getting status for projects...$(tput sgr 0)"
+
+for projectPath in `find $WSP_PATH -maxdepth 1 -type d`
 do
-	test="$(echo $D | grep -E '(module|composants)\-applicatif' -c)"
-	if [ $test -eq 1  ]; 
+    test=`echo $projectPath | grep -E "$watchPatterns" -c`
+	if [ "$test" == "1" ] ;
 	then
-	    echo
-		echo "Status for $(tput setaf 3)$(echo $D | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")$(tput sgr 0)"
-		dirPath="$path$(echo $D | sed 's/^.//')"
-		cd $dirPath
-		if [ "$1" == '-h' ] ;
+		cd $projectPath
+
+		result=`git status -s`
+        resultModified=`echo "$result" | grep -E "^(\s)?M.*"`
+        resultAdded=`echo "$result" | grep -E "^(\s)?A.*"`
+        resultDeleted=`echo "$result" | grep -E "^(\s)?D.*"`
+        resultUnknown=`echo "$result" | grep -E "^(\s)?\?.*"`
+		total=$(echo "$result" | sed '/^\s*$/d' | wc -l)
+
+		if [ "$1" == "-h" ] ;
 		then
-		    result="$(git status -s | grep -Ev 'AccreditationService|pom\.xml|\.properties|SecurityBouchonConfig|\.classpath|\.settings|\.gitignore')"
-		    nb=$(echo "$result" | sed '/^\s*$/d' | wc -l)
-		    let "modifiedFilesCount = $modifiedFilesCount + nb"
-		    echo -n "$(tput setaf 2)"
-		    echo "$result" | grep -E "^M.*"
-		    echo -n "$(tput sgr 0)$(tput setaf 1)"
-		    echo "$result" | grep -E "^\sM.*"
-		    echo -n "$(tput sgr 0)$(tput setaf 3)"
-		    echo "$result" | grep -E "^\?.*"
-		    echo -n "$(tput sgr 0)"
-		else
-            git status -s
-		    nb=$(git status -s | sed '/^\s*$/d' | wc -l)
-		    let "modifiedFilesCount = $modifiedFilesCount + nb"
+		    result=`echo "$result" | grep -Ev "AccreditationService|pom\.xml|\.properties|SecurityBouchonConfig|\.classpath|\.settings|\.gitignore"`
+            resultModified=`echo "$result" | grep -E "^(\s)?M.*"`
+            resultAdded=`echo "$result" | grep -E "^(\s)?A.*"`
+            resultDeleted=`echo "$result" | grep -E "^(\s)?D.*"`
+            resultUnknown=`echo "$result" | grep -E "^(\s)?\?.*"`
+		    total=$(echo "$result" | sed '/^\s*$/d' | wc -l)
 		fi
-		cd ../
+
+		if [ "$total" != "0" ] ;
+		then
+            echo
+            echo "$(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
+
+		    let "totalFilesCount = $totalFilesCount + $total"
+
+            if [ "$resultAdded" != "" ] ;
+            then
+                echo "$(tput setaf 2)$resultAdded$(tput sgr 0)"
+		        let "addedFilesCount = $addedFilesCount + `echo "$resultAdded" | wc -l`"
+            fi
+
+            if [ "$resultModified" != "" ] ;
+            then
+                added=`echo "$resultModified" | grep -E "^M.*"`
+                notAdded=`echo "$resultModified" | grep -E "^\sM.*"`
+
+		        let "modifiedFilesCount = $modifiedFilesCount + `echo "$resultModified" | wc -l`"
+
+                if [ "$added" != "" ] ;
+                then
+                    echo "$(tput setaf 2)$added$(tput sgr 0)"
+                fi
+                if [ "$notAdded" != "" ] ;
+                then
+                    echo "$(tput setaf 1)$notAdded$(tput sgr 0)"
+                fi
+            fi
+
+            if [ "$resultDeleted" != "" ] ;
+            then
+                echo "$(tput setaf 2)$resultDeleted$(tput sgr 0)"
+		        let "deletedFilesCount = $deletedFilesCount + `echo "$resultDeleted" | wc -l`"
+            fi
+
+            if [ "$resultUnknown" != "" ] ;
+            then
+                echo "$(tput setaf 3)$resultUnknown$(tput sgr 0)"
+		        let "mergedFilesCount = $mergedFilesCount + `echo "$resultUnknown" | wc -l`"
+            fi
+		else
+            echo "No changes in $(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
+		fi
 	fi
 done
 
 echo -e "──┬┬───────────────────────────────────────"
 echo "  ||"
-if [ "$modifiedFilesCount" == "0" ] ;
+if [ "$totalFilesCount" == "0" ] ;
 then
     echo "  ╘╧════> $(tput setaf 2)Workspace is clean !$(tput sgr 0)"
 else
-    echo "  ╘╧════> $(tput setaf 3)Modified/Added/Deleted/Merged files$(tput sgr 0)  : $modifiedFilesCount"
+    echo "  │╞════> $(tput setaf 2)Total$(tput sgr 0)    : $totalFilesCount"
+    echo "  │╞════> $(tput setaf 3)Added$(tput sgr 0)    : $addedFilesCount"
+    echo "  │╞════> $(tput setaf 3)Modified$(tput sgr 0) : $modifiedFilesCount"
+    echo "  │╞════> $(tput setaf 3)Deleted$(tput sgr 0)  : $deletedFilesCount"
+    echo "  ╘╧════> $(tput setaf 3)Merged$(tput sgr 0)   : $mergedFilesCount"
 fi
