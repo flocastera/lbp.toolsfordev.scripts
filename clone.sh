7b3a44d84859a8c2a3eea53b12ae694db9513fa4
Florian CASTERA <florian.castera@accenture.com>
2017-10-25 16:59:41 +0200

    FCA - added git clone script and syncing display

--- clone.sh
+++ clone.sh
@@ -1,21 +1,69 @@
 #!/bin/bash
 
-path="$(pwd)"
-server="https://git.sf.intra.laposte.fr/CC3_CAN/"
-gitExt=".git"
+repositoriesListFile="$WSP_PATH/repositories.txt"
+
+localProjects=`find $WSP_PATH -maxdepth 1 -type d | grep -E "$watchPatterns"`
+localProjectsName=`echo "$localProjects" | sed -e "s@$WSP_PATH/@@g"`
+
+totalReplaced=0
+totalAdded=0
+totalErrors=0
+totalIgnored=0
+
 echo
-for repo in `cat repositories.txt`
+echo "$(tput setaf 2)Cloning projects in $(tput sgr 0)$repositoriesListFile$(tput setaf 2) from depositories...$(tput sgr 0)"
+echo "─┬────────────────────────────"
+echo " │"
+
+for repo in `cat $repositoriesListFile`
 do
-	echo "Cloning repository $(tput setaf 2)$repo$(tput sgr 0)"
-	if [ "$(echo $repo | grep -E '^https' -c)" == "0" ] ;
-	then
-	    repo="$server$repo"
-	fi
-	if [ "$(echo $repo | grep -E 'git$' -c)" == "0" ] ;
-	then
-	    repo="$repo$gitExt"
-	fi
-	echo $repo
-	git clone $repo
-	echo
+    repoName=`echo "$repo" | sed -e "s@^.*/@@g" | sed -e "s@\\.git\\$@@g"`
+    exists=`echo "$localProjectsName" | grep "$repoName" -c`
+    resp=""
+    forced="0"
+    if [ "$exists" == "1" ] && [ "$1" != "-f" ] ;
+    then
+        echo "[$(tput setaf 3)o$(tput sgr 0)]──$(tput setaf 2)$repoName$(tput sgr 0) existe déjà dans le workspace ! Passage au suivant..."
+		let "totalIgnored = $totalIgnored + 1"
+    elif [ "$exists" == "1" ] ;
+    then
+        rm -rf $WSP_PATH/$repoName
+        resp=`git clone $repo $repoName 2>&1 >/dev/null`
+        forced="1"
+    else
+        resp=`git clone $repo $repoName 2>&1 >/dev/null`
+    fi
+
+    if [ `echo "$resp" | grep -ci "fatal"` -gt 0 ] ;
+    then
+        echo "[$(tput setaf 1)x$(tput sgr 0)]──$(tput setaf 2)$repoName$(tput sgr 0)"
+		let "totalErrors = $totalErrors + 1"
+        if [ `echo "$resp" | grep -ci "remote: not found"` -gt 0 ] ;
+        then
+            echo " ╞───Dépôt distant non trouvé !"
+            echo " ╞───URL : $(tput setaf 3)$repo$(tput sgr 0)"
+        fi
+        if [ `echo "$resp" | grep -ci "fatal: unable to access"` -gt 0 ] ;
+        then
+            echo " ╞───Accès au dépôt distant refusé !"
+            echo " ╞───URL : $(tput setaf 3)$repo$(tput sgr 0)"
+        fi
+    elif [ "$resp" != "" ] ;
+    then
+        if [ "$forced" == "1" ] ;
+        then
+		    let "totalReplaced = $totalReplaced + 1"
+		else
+		    let "totalAdded = $totalAdded + 1"
+        fi
+        echo "[$(tput setaf 2)V$(tput sgr 0)]──$(tput setaf 2)$repoName$(tput sgr 0) créé avec succès !"
+    fi
 done
+
+echo " │"
+echo "─╪────────────────────────────"
+echo " ╞─ $(tput setaf 3)Ignored$(tput sgr 0)  : $totalIgnored"
+echo " ╞─ $(tput setaf 2)Added$(tput sgr 0)    : $totalAdded"
+echo " ╞─ $(tput setaf 2)Replaced$(tput sgr 0) : $totalReplaced"
+echo " ╞─ $(tput setaf 1)Errors $(tput sgr 0)  : $totalErrors"
+echo " ╘────────────────────────────"
--- status.sh
+++ status.sh
@@ -82,7 +82,7 @@ do
             fi
 		else
 		    # Displaying message if nothing change in folder (supports excluded files)
-            echo "[$(tput setaf 2)V$(tput sgr 0)] No changes in $(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
+            echo "[$(tput setaf 2)V$(tput sgr 0)]──No changes in $(echo $projectPath | grep -Eo "(H[0-9]{2}\-)?[a-zA-Z]*$")"
 		fi
 	fi
 done
