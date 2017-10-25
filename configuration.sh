11d9d6c61d5a6a18b0f805476454d5b00f82661d
Florian CASTERA <florian.castera@accenture.com>
2017-10-25 17:45:23 +0200

    FCA - added comments in configuration file

--- configuration.sh
+++ configuration.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Pour cette variable :
+#   soit la renseigner directement ici sous la forme de chemin absolu
+#   soit la renseigner dans les variables d'environnement
+# Cette variable doit obligatoirement être valorisé car tous les scripts l'utilisent pour
+# lister les différents projets
+# Exemple de valorisation dans ce fichier :
+#   export WSP_PATH="D:/DATA/ESPDEV/cc3env/workspaceJDK8/"
 export WSP_PATH=$WSP_PATH
+
 export SCRIPTS_PATH=$WSP_PATH/lbp.toolsfordev.scripts
 
 export projectNamePatterns="(H[0-9]{2}\-)?[a-zA-Z]*$"
